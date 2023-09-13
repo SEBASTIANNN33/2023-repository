01e14ba37b5f55a67848dee8b01f41e32e6905f9
SEBASTIANNN33 <144904121+SEBASTIANNN33@users.noreply.github.com>
2023-09-13 12:02:28 +0000

    01

---
+++ 01.py
@@ -0,0 +1,19 @@
+print()
+print(1)
+print()
+
+vypis = print
+
+
+
+def vypis(x):
+    print(x)
+
+# def vydel(delenec, delenec):
+#     return delenec / delenec
+
+def soucet (a, b):
+    return a + b
+
+vysledek = soucet(19, 23)
+print(vysledek)
